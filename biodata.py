17492d2966c326c34570f4153a583206fad78344
Budimanefenfi <164857179+Budimanefenfi@users.noreply.github.com>
2024-03-28 20:52:07 +0800

    BIODATA

---
+++ biodata.py
@@ -0,0 +1,28 @@
+#python 3.7.1
+
+print ("Hello, Dcoder!")
+print("\n Data Diri Mahasiswa")
+
+nama=input("Masukkan Nama : ")
+kelamin=input("Masukan kelamin")
+nim=input("Masukkan Nim Anda : ")
+prodi=input("Masukkan Prodi : ")
+alamat=input("Masukkan Alamat : ")
+usia=int(input("Masukkan Usia : "))
+tanggal_lahir=int(input("Masukkan Tanggal Lahir : "))
+bulan_lahir=int(input("Masukkan Bulan Lahir : "))
+tahun_lahir=int(input("Masukkan Tahun Lahir : "))
+
+print("=="*23)
+print("Nama :",nama)
+print("kelamin:")
+print("Nim :",nim)
+print("Prodi :",prodi)
+print("Alamat :",alamat)
+print("Usia :",usia)
+print("Tanggal Lahir ",tanggal_lahir)
+print("Bulan Lahir :",bulan_lahir)
+print("Tahun Lahir :",tahun_lahir)
+print("=="*23)
+
+print("\n Terima Kasih")
